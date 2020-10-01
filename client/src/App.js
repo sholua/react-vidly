58b7dfe39bde7ad71795d54c5a6d7b3101c8a870
Oleksandr Shevchuk <s471652@gmail.com>
2020-10-01 12:38:27 +0300

    #179 Logging out the user

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import NotFound from "./components/notFound";
 import NavBar from "./components/common/navBar";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
+import Logout from "./components/common/logout";
 import RegisterForm from "./components/registerForm";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -32,6 +33,7 @@ class App extends Component {
           <Switch>
             <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
