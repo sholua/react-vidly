96bb2586a7451cc5a9d06e76c08ad547b314a381
Oleksandr Shevchuk <s471652@gmail.com>
2020-10-02 06:53:41 +0300

    #181 Calling protected API endpoints

--- client/src/services/httpService.js
+++ client/src/services/httpService.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 import logger from "./logService";
+import auth from "./authService";
+
+axios.defaults.headers.common["x-auth-token"] = auth.getJwt();
 
 axios.interceptors.response.use(null, (error) => {
   const expectedErrors =
