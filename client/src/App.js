c8ce8432190e9b3fe5d445ea4b35ba57cb3e5c90
Oleksandr Shevchuk <s471652@gmail.com>
2020-10-02 09:03:40 +0300

    #186 Common protected route

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/common/navBar";
+import ProtectedRoute from "./components/common/protectedRoute";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import Logout from "./components/logout";
@@ -33,13 +34,7 @@ class App extends Component {
             <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
             <Route path="/logout" component={Logout} />
-            <Route
-              path="/movies/:id"
-              render={(props) => {
-                if (!user) return <Redirect to="/login" />;
-                else return <MovieForm {...props} />;
-              }}
-            />
+            <ProtectedRoute path="/movies/:id" component={MovieForm} />
             <Route
               path="/movies"
               render={(props) => <Movies {...props} user={user} />}
